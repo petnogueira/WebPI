b01a572d1559e8d59999ef85839311e242f6c746
Peterson <petnogueira@gmail.com>
2017-05-18 20:50:24 -0300

    Change control for file

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
-
+$pathFile = "../py/teste.txt";
 if ($_GET["c"] == "y"){
 	echo "Process Started";
-	system("sudo python ../py/blink.py");
+	$myfile = fopen($pathFile, "w");
+	fwrite($myfile, "on");
+	fclose($myfile);
 }
 if ($_GET["c"] == "n"){
 	echo "Process Stoped";
-	system("sudo pkill -9 'python'");
+	$myfile = fopen($pathFile, "w");
+	fwrite($myfile, "off");
+	fclose($myfile);
 }
 
 ?>
--- py/blink.py
+++ py/blink.py
@@ -3,11 +3,23 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 
-try:
-    while True:
-    	GPIO.output(13, True)
-        time.sleep(0.5)
-        GPIO.output(13, False)
-        time.sleep(0.5)
-except KeyboardInterrupt:
+def on():
+    GPIO.output(13, True)
+    time.sleep(0.5)
+    GPIO.output(13, False)
+    time.sleep(0.5)
+    return
+
+def off():
     GPIO.cleanup()
+    return
+
+try:
+    f = open('teste.txt', 'r+')
+    while True:
+        f.seek(0)
+        resp = f.read()
+        if (resp == "on"):
+            on()
+except KeyboardInterrupt:
+    off()
