2027a493bb8b5cfe7fe813d05780d35651d422a8
Peterson <petnogueira@gmail.com>
2017-05-22 00:09:27 -0300

    Changes:
    - Add include folder and header.php
    - IHM

--- public/index.php
+++ public/index.php
@@ -1,16 +1,29 @@
-<?php
-$pathFile = "../py/teste.txt";
-if ($_GET["c"] == "y"){
-	echo "Process Started";
-	$myfile = fopen($pathFile, "w");
-	fwrite($myfile, "on");
-	fclose($myfile);
-}
-if ($_GET["c"] == "n"){
-	echo "Process Stoped";
-	$myfile = fopen($pathFile, "w");
-	fwrite($myfile, "off");
-	fclose($myfile);
-}
 
+<html>
+<?php 
+    include "../inc/header.php";
+
+    $pathFile = "../py/teste.txt";
+    if (isset($_POST["ck"])){
+        $myfile = fopen($pathFile, "w");
+        fwrite($myfile, "on");
+        fclose($myfile);
+    }else{
+        $myfile = fopen($pathFile, "w");
+        fwrite($myfile, "off");
+        fclose($myfile);
+    }
 ?>
+<body>
+<div class="container">
+  <h2>Blink</h2>
+  <form id="blink" action="index.php" method="post">
+    <p>Blink: </p>
+    <div class="checkbox">
+      <label><input type="checkbox" value="" name="ck" <?php if (isset($_POST["ck"])) echo "checked";?>>Active</label>
+    </div>
+    <input type="submit" name="submit" value="Submit" />
+  </form>
+</div>
+</body>
+</html>
